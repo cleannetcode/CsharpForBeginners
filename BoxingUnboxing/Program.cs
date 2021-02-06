c554e94715ada13b36556cddc5a41789ac07017c
Trufanov Roman <pingvin1308@gmail.com>
2021-02-06 14:18:13 +0300

    add project to test boxing and unboxing

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
